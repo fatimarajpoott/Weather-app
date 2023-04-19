7dac726d128b52765448a22b8f19ee0acf8c0ff2
Fatima Shafiq <fatimarajpoott@gmail.com>
2023-04-19 15:53:19 +0500

    second-commit

--- script.js
+++ script.js
@@ -65,19 +65,6 @@ function displayWeatherData(data) {
   humidity.innerHTML = `${data.main.humidity}%`;
 
   windSpeed.innerHTML = `${data.wind.speed} Km/H`;
-
-//   if (data.weather[0].main == "Clouds") {
-//     weatherIcon.src = "clouds.png";
-//   } else if (data.weather[0].main == "Clear") {
-//     weatherIcon.src = "clear.png";
-//   } else if (data.weather[0].main == "Rain") {
-//     weatherIcon.src = "rain.png";
-//   } else if (data.weather[0].main == "Drizzle") {
-//     weatherIcon.src = "drizzle.png";
-//   } else if (data.weather[0].main == "Mist") {
-//     weatherIcon.src = "mist.png";
-//   }
-
   
 }
 
